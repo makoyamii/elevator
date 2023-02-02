bc8a679e4827534ebc2348c45fcb9fc989630698
mako <sanjana.ravikumar77@gmail.com>
2023-02-02 17:22:11 -0600

    Add empty project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+
+    void FixedUpdate() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
